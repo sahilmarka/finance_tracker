e490bc4e620a40507477114ae1831288d7079a14
sahilmarka <m.sahilkumar1234@gmail.com>
2021-07-12 00:20:23 +0530

    add AJAX and improve UX

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,9 +2,13 @@ class Stock < ApplicationRecord
 
 	def self.new_lookup(ticker_symbol) 
 		client = IEX::Api::Client.new(  publishable_token: Rails.application.credentials.iex_client[:publishable_key],
-  																  secret_token: Rails.application.credentials.iex_client[:secret_key],
-  																	endpoint: 'https://sandbox.iexapis.com/v1'  )
-		client.price(ticker_symbol)
+  									                secret_token: Rails.application.credentials.iex_client[:secret_key],
+  											            endpoint: 'https://sandbox.iexapis.com/v1'  )
+		begin																
+			new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+		rescue => exception 
+			return nil 
+		end
 	end
 
 end
